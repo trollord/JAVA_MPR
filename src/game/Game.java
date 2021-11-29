9f3e104a78afd72b79596389e9e893cadf697723
trollord <80673827+trollord@users.noreply.github.com>
2021-11-30 00:01:23 +0530

    Updated the sql for the file game, made three data base and 2 subtables in each

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,29 +1,56 @@
 package game;
 
 import java.util.Scanner;
-
 import auth.User;
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
-public abstract class Game
-{
+//maze_game  rock_paper_scissor  x_and_o
+public abstract class Game {
+
+	String url = "jdbc:mysql://localhost:3306/";
+	String db_user = "root";
+	String passwd = "Mandar@165";
 	protected final String GAME_NAME;
 	protected User user;
-	
-	public Game(String GAME_NAME, User user)
-	{
+
+	public Game(String GAME_NAME, User user) {
 		this.GAME_NAME = GAME_NAME;
 		this.user = user;
 	}
-	
-	protected void registerScore(int value)
-	{
-		
+
+	protected void registerScore(int value) {
+
+		try {
+			Connection con = DriverManager.getConnection((url + GAME_NAME), user, passwd);
+			String s1 = "insert into data(p_name,p_score) values(?,?)";
+			PreparedStatement pstmt = con.prepareStatement(s1);
+			pstmt.setString(1, user);
+			pstmt.setInt(2, value);
+			pstmt.executeUpdate();
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
 	}
-	
-	protected void displayLeaderboard()
-	{
-		
+
+	protected void displayLeaderboard() {
+		System.out.println("*********_THE LEADERBOARD_**********");
+		System.out.println("NAME\t--" + "POINTS\t");
+		System.out.printf("_____________________________________\n");
+		Connection con = DriverManager.getConnection((url + GAME_NAME), user, passwd);
+		String query = "SELECT * FROM data ORDER BY p_score DESC LIMIT 5";
+		Statement stmt = con.createStatement();
+		ResultSet rs2 = stmt.executeQuery(query);
+		while (rs2.next()) {
+			String one = rs2.getString("p_name");
+			Integer two = rs2.getInt("p_score");
+			System.out.println(one + "\t--" + two + "\t");
+		}
+
 	}
-	
+
 	protected abstract void start(Scanner sc);
 }
